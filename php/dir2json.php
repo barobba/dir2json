3403f5badf9a7dfd8532e63a24955a8cd7f822b2
Biagio Arobba <barobba@gmail.com>
2012-05-02 12:39:14 -0600

    Initial code.
    Signed-off-by: Biagio Arobba <barobba@gmail.com>

---
+++ php/dir2json.php
@@ -0,0 +1,42 @@
+<?php
+
+//
+// Driver
+//
+
+if (count($argv) > 2) {
+  folder_array($argv[$argc-1]);
+}
+else {
+  print "Usage: dir2json PATH";
+}
+
+
+//
+// Functions
+//
+
+function folder_array($filepath) {
+  return _folder_array_recurse($filepath, $filepath);
+}
+
+function _folder_array_recurse($filename, $filepath) {
+  // NOTE: "IS_DIR()" WASN'T WORKING ON LINUX (OR POSSIBLY PHP VERSION).
+  if (is_file($filepath)) {
+    // BASIS CASE
+    return $filename;
+  }
+  else {
+    // RECURSIVE CASE
+    $folders = array();
+    $folder_contents = scandir($filepath);
+    foreach ($folder_contents as $sub_filename) {
+      // Skip the . and ..
+      if ($sub_filename == '.' || $sub_filename == '..') {
+        continue;
+      }
+      $folders[$sub_filename] = _folder_array_recurse($sub_filename, $filepath.'/'.$sub_filename);
+    }
+    return $folders;
+  }
+}
